759e3f4cbc7fe980315e59052dd690ee85759f65
Soumadip Das <soumadipd43@gmail.com>
2026-09-19 05:54:27 +0530

    Simplify base URL configuration in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: process.env.NODE_ENV === 'production' ? '/Soumadip-Das---new-portfolio/' : '/',
+    base:'/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
